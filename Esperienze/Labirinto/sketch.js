8beb5ea9413a2555bd28c3b05ef4611b6286dcd2
PaoloGinefra <ginefra.paolo@gmail.com>
2022-06-29 00:52:25 +0200

    Some exemples

--- Esperienze/Labirinto/sketch.js
+++ Esperienze/Labirinto/sketch.js
@@ -1,10 +1,42 @@
+// Automabot Exemple variables
+/*
+const Automaton = [
+	[0, 1],
+	[0, 1],
+]
+
+let Nodes = [];
+
+let automabot;
+*/
+
+
 function setup() {
+	World.width = cols * cellSize;
+	World.height = rows * cellSize;
 	World.setup();
-	createCanvas(cols * cellSize, rows * cellSize);
 	textSize(32);
 	textAlign(CENTER, CENTER);
 	generateMaze();
 
+	if (stack.length === 0) {
+		background(60);
+		const Automaton = result();
+		printMatrix(Automaton)
+		console.log("Maze Generated")
+		let shortestWord = ShortestWord(Automaton);
+		console.log(shortestWord)
+		console.log(WordInterpreter(shortestWord[0]))
+	}
+
+	//Aautomabot Exemple Setup
+	/*
+	Nodes[0] = createVector(-1, 0);
+	Nodes[1] = createVector(1, 0);
+
+	automabot = new Automabot(Automaton, Nodes, 0.2, 0.1, Automabot.Sin)
+
+	automabot.computeAnimation(0, "10010")*/
 }
 
 const Directions = ['N', 'E', 'S', 'W']
@@ -18,14 +50,17 @@ function WordInterpreter(word){
 }
 
 function draw() {
-	if (stack.length === 0) {
-		background(60);
-		noLoop();
-		const Automaton = result();
-		let shortestWord = ShortestWord(Automaton);
-		console.log(shortestWord)
-		console.log(WordInterpreter(shortestWord[0]))
-	}
+	World.draw()
+	
+	//Aautomabot Exemple Draw
+	/*background(0)
+
+	Nodes.forEach(node => {
+		let p = World.w2s(node);
+		ellipse(p.x, p.y, 0.3 * World.w2s())
+	});
+
+	automabot.draw()*/
 }
 
 
