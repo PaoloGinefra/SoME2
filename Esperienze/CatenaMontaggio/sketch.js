c1f7e102069a9dab09f6257132c80665990fbce9
darcros <darcros7@gmail.com>
2022-06-29 14:06:43 +0200

    (CatenaMontaggio) write my own physics classes

--- Esperienze/CatenaMontaggio/sketch.js
+++ Esperienze/CatenaMontaggio/sketch.js
@@ -1,37 +1,106 @@
 import "p5";
-import { Engine } from "matter-js";
-
-// where i stole the classes from: https://github.com/b-g/p5-matter-examples/
-import { Block } from "./Block";
+import { Bodies, Engine, Vertices, World } from "matter-js";
 
 // enable intellisense autocompletion for p5 globals
 /// <reference path="@types/p5/global.d.ts" />
 
+class PhysicsBody {
+  constructor(world, attributes, options) {
+    this.world = world;
+    this.attributes = attributes;
+    this.options = options;
+    this.createBody();
+    World.add(this.world, this.body);
+  }
+
+  createBody() {
+    throw new Error("Not implemented");
+  }
+
+  draw() {
+    if (this.attributes.color) {
+      fill(this.attributes.color);
+    } else {
+      noFill();
+    }
+
+    if (this.attributes.stroke) {
+      stroke(this.attributes.stroke);
+      if (this.attributes.weight) {
+        strokeWeight(this.attributes.weight);
+      }
+    } else {
+      noStroke();
+    }
+
+    this.drawVertices(this.body.vertices);
+  }
+
+  drawVertices(vertices) {
+    beginShape();
+    for (const vertice of vertices) {
+      vertex(vertice.x, vertice.y);
+    }
+    endShape(CLOSE);
+  }
+}
+
+class Box extends PhysicsBody {
+  constructor(world, attributes, options) {
+    super(world, attributes, options);
+  }
+
+  createBody() {
+    this.body = Bodies.rectangle(
+      this.attributes.x,
+      this.attributes.y,
+      this.attributes.w,
+      this.attributes.h,
+      this.options
+    );
+  }
+}
+
+class Polygon extends PhysicsBody {
+  constructor(world, attributes, options) {
+    super(world, attributes, options);
+  }
+
+  createBody() {
+    this.body = Bodies.fromVertices(
+      this.attributes.x,
+      this.attributes.y,
+      this.attributes.vertexSet,
+      this.options
+    );
+  }
+}
+
 let engine = Engine.create();
 let world = engine.world;
 
-let blockA;
-let blockB;
+let poly;
 let ground;
 
 window.setup = function () {
   createCanvas(700, 500);
 
-  blockA = new Block(world, {
+  // FIXME: it does not look anything like an arrow
+  let arrowVertices = Vertices.fromPath(
+    "40 0 40 20 100 20 100 80 40 80 40 100 0 50"
+  );
+
+  // let chevronVertices = Vertices.fromPath(
+  //   "100 0 75 50 100 100 25 100 0 50 25 0"
+  // );
+
+  poly = new Polygon(world, {
     x: 200,
     y: 200,
-    w: 80,
-    h: 80,
+    vertexSet: arrowVertices,
     color: "white",
   });
-  blockB = new Block(world, {
-    x: 270,
-    y: 50,
-    w: 160,
-    h: 80,
-    color: "white",
-  });
-  ground = new Block(
+  ground = new Box(
     world,
     { x: 400, y: 500, w: 810, h: 15, color: "grey" },
     { isStatic: true, angle: PI / 36 }
@@ -44,7 +113,6 @@ window.draw = function () {
 
   background("#333");
 
-  blockA.draw();
-  blockB.draw();
+  poly.draw();
   ground.draw();
 };
