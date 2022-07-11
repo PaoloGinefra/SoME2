ed76fbad5eb29c8bce40f0ab351eb4eb60b9f3ea
Federico Grandi <fgrandi30@gmail.com>
2022-07-11 15:33:56 +0000

    chore: remove website

--- website/src/components/TextContainer.js
+++
@@ -1,7 +0,0 @@
-import classes from '../styles/TextContainer.module.css'
-
-const TextContainer = ({ children }) => {
-  return <div className={classes.container}>{children}</div>
-}
-
-export default TextContainer
