20256366c23f905ab9b00efc1ef6be32dc4e53ef
PaoloGinefra <ginefra.paolo@gmail.com>
2022-07-20 22:18:45 +0200

    Background color component

---
+++ src/components/FadeBg.tsx
@@ -0,0 +1,28 @@
+import { Color } from 'p5'
+import classes from '../styles/FadeBg.module.css'
+
+const FadeBg = (props) => {
+  return (
+    <>
+      {/* <div
+        className={classes['fade-top']}
+        style={{
+          background:
+            'linear-gradient(to top,' + props.color + ', rgba(0, 0, 0, 0))',
+        }}
+      /> */}
+      <div className={classes.bg} style={{ backgroundColor: props.color }}>
+        {props.children}
+      </div>
+      {/* <div
+        className={classes['fade-bottom']}
+        style={{
+          background:
+            'linear-gradient(to bottom,' + props.color + ', rgba(0, 0, 0, 0))',
+        }}
+      /> */}
+    </>
+  )
+}
+
+export default FadeBg
