31f2889b737d2e742f0b179a3b1cf0d91e606737
PaoloGinefra <ginefra.paolo@gmail.com>
2022-07-22 15:08:29 +0200

    Minor buttons tweeks

--- public/sketches/conveyor-belt/js/settings.js
+++ public/sketches/conveyor-belt/js/settings.js
@@ -1,6 +1,6 @@
 const CONVEYOR_SPEED = 0.5
-const INITIAL_SECTIONS = 7
-const MIN_SECTIONS = 2
+const INITIAL_SECTIONS = 1
+const MIN_SECTIONS = 1
 const MAX_SECTIONS = 10
 const SECTION_WIDTH = 400 // px
 const DEBUG = false
