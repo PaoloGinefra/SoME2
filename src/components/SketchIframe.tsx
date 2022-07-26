a993f1d323bd351be0cd2f6f0eb02ffd81ccfcb3
darcros <darcros7@gmail.com>
2022-07-26 22:58:24 +0200

    feat: iframe padded option + border radius

--- src/components/SketchIframe.tsx
+++ src/components/SketchIframe.tsx
@@ -3,15 +3,22 @@ import classes from '../styles/SketchIframe.module.css'
 interface SketchIframeProps {
   src: string
   height: number
+  padded?: boolean
 }
 
-const SketchIframe = ({ src, height }: SketchIframeProps) => (
-  <iframe
-    src={src}
-    style={{ height }}
-    className={classes.frame}
-    frameBorder={0}
-  ></iframe>
-)
+const SketchIframe = ({ src, height, padded = false }: SketchIframeProps) => {
+  const className = [classes.frame, padded && classes.padded]
+    .filter((x) => !!x)
+    .join(' ')
+
+  return (
+    <iframe
+      src={src}
+      style={{ height }}
+      className={className}
+      frameBorder={0}
+    ></iframe>
+  )
+}
 
 export default SketchIframe
