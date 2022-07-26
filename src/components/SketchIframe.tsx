54c37ad8fc9182902b7a18f9368ff5b3faae8dc1
darcros <darcros7@gmail.com>
2022-07-26 23:55:18 +0200

    feat: sketch iframe aspectRatio

--- src/components/SketchIframe.tsx
+++ src/components/SketchIframe.tsx
@@ -1,18 +1,47 @@
+import { useEffect, useReducer, useRef, useState } from 'react'
 import classes from '../styles/SketchIframe.module.css'
 
 interface SketchIframeProps {
   src: string
-  height: number
+  initialHeigth: number
+
+  // aspect ration expressed as width/height
+  // if not defined the iframe will use `width` as width
+  aspectRatio?: number
+
   padded?: boolean
 }
 
-const SketchIframe = ({ src, height, padded = false }: SketchIframeProps) => {
+const SketchIframe = ({
+  src,
+  initialHeigth,
+  aspectRatio,
+  padded = false,
+}: SketchIframeProps) => {
+  const ref = useRef<HTMLIFrameElement>()
+  const [height, setHeigth] = useState(initialHeigth)
+
   const className = [classes.frame, padded && classes.padded]
     .filter((x) => !!x)
     .join(' ')
 
+  useEffect(() => {
+    if (!aspectRatio) return
+
+    const doResize = () => {
+      const w = ref.current.clientWidth
+      const h = w / aspectRatio 
+      setHeigth(h)
+    }
+    doResize()
+
+    window.addEventListener('resize', doResize)
+    return () => window.removeEventListener('resize', doResize)
+  })
+
   return (
     <iframe
+      ref={ref}
       src={src}
       style={{ height }}
       className={className}
