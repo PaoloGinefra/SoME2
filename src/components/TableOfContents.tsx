de34533bb38dffbc58f0a049b4cb7e3e887a1913
darcros <darcros7@gmail.com>
2022-07-28 14:24:31 +0200

    feat: move toc content inside mdx

--- src/components/TableOfContents.tsx
+++ src/components/TableOfContents.tsx
@@ -2,26 +2,15 @@ import classes from '../styles/TableOfContents.module.css'
 
 type TocEntry = string | { [key: string]: TocEntry }
 
-const toc: TocEntry = {
-  'An adventurous mathematician': '#an-adventurous-mathematician',
-  'An orientation problem fair and square':
-    '#an-orientation-problem-fair-and-square',
-  "What's behind the trick": '#whats-behind-the-trick',
-  '.': {
-    'Time to abstract': '#time-to-abstract',
-    'When words are sharper than bits': '#when-words-are-sharper-than-bits',
-    'Reasoning about the unknown': '#reasoning-about-the-unknown',
-    "There's more than mines and lego bricks":
-      '#theres-more-than-mines-and-lego-bricks',
-  },
-  Credits: '#the-world-behind-the-keyboard',
+interface TableOfContentsProps {
+  toc: TocEntry
 }
 
-export default function TableOfContents() {
-  const parse = (abs: TocEntry) => {
-    const listKey = Object.values(abs).join(' ')
+export default function TableOfContents({ toc }: TableOfContentsProps) {
+  const parse = (tocEntry: TocEntry) => {
+    const listKey = Object.values(tocEntry).join(' ')
 
-    const listEntries = Object.entries(abs).map(([title, href]) =>
+    const listEntries = Object.entries(tocEntry).map(([title, href]) =>
       typeof href == 'string' ? (
         <li key={href} className={classes.li}>
           <a className={classes.a} href={href}>
@@ -39,6 +28,7 @@ export default function TableOfContents() {
       </ul>
     )
   }
+
   return (
     <div className={classes.toc}>
       <p className={classes.p}>Table of Contents:</p>
