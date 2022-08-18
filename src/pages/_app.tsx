5a97d5ab0f8ed9cfe1724b65f582d358b1c47a37
PaoloGinefra <ginefra.paolo@gmail.com>
2022-08-18 18:27:18 +0200

    feat: added the site's link preview

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -18,12 +18,7 @@ function MyApp({ Component, pageProps }: AppProps) {
             'A SoME2 submission about the so-called "Road coloring problem"'
           }
         />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
+        <meta property="og:image" content={`./public/img/copertina.png`} />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
         <title>{siteTitle}</title>
