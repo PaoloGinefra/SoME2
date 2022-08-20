5320c928da79d34cab7cb8325a4ca0e5b98b9ec3
darcros <darcros7@gmail.com>
2022-08-20 15:57:42 +0200

    update mobile modal text

--- src/components/MobileModal.tsx
+++ src/components/MobileModal.tsx
@@ -17,8 +17,17 @@ export default function MobileModal() {
     <Modal open={open} onClose={() => setOpen(false)}>
       <p>
         Hi, it looks like you are using a mobile device (or at least a device
-        with a screen that isn&apos;t very wide); this website is best viewed on
-        a PC.
+        with a screen that isn&apos;t very wide).
+      </p>
+
+      <p>
+        This article features some interactive experiences which are optimized
+        for PCs, so the experience on smartphones is a little rough around the
+        edges. (They still work, just not very well)
+      </p>
+      <p>
+        But dont&apos;t worry! You can still read the text and try out the
+        interactive parts later on a PC.
       </p>
     </Modal>
   )
